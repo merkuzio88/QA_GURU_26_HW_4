91f5f724016744309ef3928813ff228f7734bbb5
Pavel Kulikov <pkulikov@secureducation.ru>
2024-03-15 09:59:55 +0700

    add full text check

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -20,6 +20,17 @@ public class SelenideTest {
         $$("button").findBy(text("Show 3 more pages")).click();
         $$(".details-reset a").findBy(text("SoftAssertions")).scrollIntoView(true).click();
         $$(".markdown-heading").findBy(text("JUnit5")).sibling(0).scrollIntoView(true)
-                .shouldHave(cssClass("highlight-source-java")).shouldHave(text("@ExtendWith({SoftAssertsExtension.class})"));
+                .shouldHave(cssClass("highlight-source-java"))
+                .shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}"));
     }
 }
